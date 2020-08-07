75ad05f30cb3e890d9524743a45dab7491d4d53a
Oscar Wales <oscarjwales@gmail.com>
2020-08-07 12:18:35 +0100

    Added typed to Alphabet challenge

--- app/Tricksy/Alphabet.php
+++ app/Tricksy/Alphabet.php
@@ -9,31 +9,31 @@ class Alphabet implements Iterator
     private $position = 65;
     private $key = 0;
     
-    public function rewind()
+    public function rewind() : Alphabet
     {
         $this->position = 65;
         $this->key = 0;
         return $this;
     }
     
-    public function current() 
+    public function current() : string
     {
         return chr($this->position);
     }
 
-    public function key()
+    public function key() : int
     {
         return $this->key;
     }
     
-    public function next()
+    public function next() : Alphabet
     {
         $this->position +=1;
         $this->key +=1;
         return $this;
     }
 
-    public function valid()
+    public function valid() : int
     {
         return $this->position < (65+26);
     }
